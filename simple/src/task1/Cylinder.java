6c8f062be28e82478c2aad3e21c8610311fe4875
panmari <smmuzi@gmail.com>
2012-09-23 16:45:57 +0200

    added mantle, color & some helper

--- simple/src/task1/Cylinder.java
+++ simple/src/task1/Cylinder.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.vecmath.Matrix3f;
 import javax.vecmath.Point3f;
+import javax.vecmath.Tuple3f;
 
 import jogamp.graph.math.MathFloat;
 import jrtr.VertexData;
@@ -30,29 +31,50 @@ public class Cylinder extends AbstractShape {
 		this.lowerDiscCenterVertex = verticesList.size()/3;
 		addDisc(-height/2);
 		addElement(toFloatArray(verticesList), VertexData.Semantic.POSITION, 3);
+		addElement(toFloatArray(colorsList), VertexData.Semantic.COLOR, 3);
+		for (int i = 1; i <= resolution; i++) {
+			addIndex(upperDiscCenterVertex, i, getAdjacentCircleVertex(i));
+		}
 		
-		for (int i = 1; i < resolution; i++) {
-			addIndex(upperDiscCenterVertex, i, i + 1);
-		}
-		addIndex(upperDiscCenterVertex, upperDiscCenterVertex + 1, resolution);
 		for (int i = lowerDiscCenterVertex + 1; i < getNumberOfVertices() - 1; i++) {
-			addIndex(lowerDiscCenterVertex, i, i+1);
+			addIndex(lowerDiscCenterVertex, i, getAdjacentCircleVertex(i));
 		}
-		addIndex(lowerDiscCenterVertex, lowerDiscCenterVertex + 1, getNumberOfVertices()-1);
+		addMantle();
 		addIndicesList(indicesList);
 	}
 	
+	/**
+	 * Adds a disc to the mesh centered around the z-axis on 
+	 * the given zcoordinate. Radius & resolution are taken from
+	 * instance variables
+	 * @param zcoordinate
+	 */
 	private void addDisc(float zcoordinate) {
 		Point3f centerCircle = new Point3f(0, 0, zcoordinate);
 		Matrix3f rotationMatrix = new Matrix3f();
 		rotationMatrix.rotZ(2*MathFloat.PI/resolution);
 		addVertex(centerCircle); //center of circle is first point in mesh
+		addColor(0, 0, 0);
 		Point3f p = new Point3f(radius, 0, 0); //first point of circle
 		for (int i = 0; i < resolution; i++) {
 			Point3f meshPoint = new Point3f();
 			meshPoint.add(p, centerCircle);
 			addVertex(meshPoint);
+			addColor(i % 2, i % 2, i % 2);
 			rotationMatrix.transform(p);
 		}
+	}
+	
+	private void addMantle() {
+		for (int i = 1; i <= resolution; i++) {
+			addIndex(i, getAdjacentCircleVertex(i), i + lowerDiscCenterVertex);
+			addIndex(getAdjacentCircleVertex(i), getAdjacentCircleVertex(i) + lowerDiscCenterVertex, i + lowerDiscCenterVertex);
+		}
+	}
+	
+	private int getAdjacentCircleVertex(int vertex) {
+		if (vertex % resolution == 0)
+			return 1;
+		else return  vertex + 1;
 	}
 }
