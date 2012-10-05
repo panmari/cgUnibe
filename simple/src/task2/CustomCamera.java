bee20b6b03927772c2cedb405bfbcae602cc7089
panmari <smmuzi@gmail.com>
2012-10-05 17:50:56 +0200

    added constructor for customCamera

--- simple/src/task2/CustomCamera.java
+++ simple/src/task2/CustomCamera.java
@@ -7,7 +7,13 @@ import jrtr.Camera;
 
 public class CustomCamera extends Camera {
 
+	private Point3f centerOfProjection;
+	private Point3f lookUpPoint;
+	private Vector3f upVector;
+
 	public CustomCamera(Point3f centerOfProjection, Point3f lookUpPoint, Vector3f upVector) {
-		
+		this.centerOfProjection = centerOfProjection;
+		this.lookUpPoint = lookUpPoint;
+		this.upVector = upVector;
 	}
 }
