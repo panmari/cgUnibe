ce2cb7564e25fa7eff7d580761510ed5a725b799
panmari <smmuzi@gmail.com>
2012-10-27 19:09:12 +0200

    was slow, doing now bitshift!

--- jrtr/src/jrtr/SWTexture.java
+++ jrtr/src/jrtr/SWTexture.java
@@ -30,37 +30,48 @@ public class SWTexture implements Texture {
 	}
 
 	/**
-	 * this is so ugly, refactor pls!
+	 * this is ugly & slow, refactor pls!
 	 * @param x
 	 * @param y
 	 * @return
 	 */
 	public int getBilinearInterpolatedColor(float x, float y) {
 		Point2f scaled = new Point2f(getScaledX(x), getScaledY(y));
-		Color[] top = new Color[2], bottom = new Color[2];
-		top[0] = new Color(texture.getRGB(floor(scaled.x), floor(scaled.y)));
-		top[1] = new Color(texture.getRGB(ceil(scaled.x), floor(scaled.y)));
-		bottom[0] = new Color(texture.getRGB(floor(scaled.x), ceil(scaled.y)));
-		bottom[1] = new Color(texture.getRGB(ceil(scaled.x), ceil(scaled.y)));
+		int[] top = new int[2], bottom = new int[2];
+		//this step is very bad
+		top[0] = texture.getRGB(floor(scaled.x), floor(scaled.y));
+		top[1] = texture.getRGB(ceil(scaled.x), floor(scaled.y));
+		bottom[0] = texture.getRGB(floor(scaled.x), ceil(scaled.y));
+		bottom[1] = texture.getRGB(ceil(scaled.x), ceil(scaled.y));
 		float horzCoeff = ceil(scaled.x) - scaled.x;
-		float[] left = new float[3], right = new float[3];
-		top[0].getColorComponents(left);
-		top[1].getColorComponents(right);
-		float[] avgTop = interpolateBetween(left, right, horzCoeff);
-		bottom[0].getColorComponents(left);
-		bottom[1].getColorComponents(right);
-		float[] avgBottom = interpolateBetween(left, right, horzCoeff);
+		int[] left = getRGBArrayOfHexaColor(top[0]);
+		int[] right = getRGBArrayOfHexaColor(top[1]);
+		int[] avgTop = interpolateBetween(left, right, horzCoeff);
+		left = getRGBArrayOfHexaColor(bottom[0]);
+		right = getRGBArrayOfHexaColor(bottom[1]);
+		int[] avgBottom = interpolateBetween(left, right, horzCoeff);
 		float vertCoeff = ceil(scaled.y) - scaled.y;
-		float[] result = interpolateBetween(avgTop, avgBottom, vertCoeff);
-		return new Color(top[0].getColorSpace(), result, 1).getRGB();
+		int[] result = interpolateBetween(avgTop, avgBottom, vertCoeff);
+		
+		return (result[0] << 16) & (result[1] << 8) & result[2];
 	}
 	
-	private float[] interpolateBetween(float[] colorNear, float[] colorFar, float coeff) {
-		float[] avg = new float[3];
+	private int[] interpolateBetween(int[] colorNear, int[] colorFar, float coeff) {
+		int[] avg = new int[3];
 		for (int i = 0; i < colorNear.length; i++) {
-			avg[i] = colorNear[i]*coeff + colorFar[i]*(1-coeff);
+			avg[i] = (int)(colorNear[i]*coeff + colorFar[i]*(1-coeff));
 		}
 		return avg;
+	}
+	
+	private int[] getRGBArrayOfHexaColor(int hexaColor) {
+		int[] rgb = new int[3];
+		int bitmask = 0xFF0000;
+		for (int i = 0; i < 3; i++) {
+			rgb[i] = hexaColor & bitmask;
+			bitmask = bitmask >> 8;
+		}
+		return rgb;
 	}
 	
 	private float getScaledX(float x) {
@@ -74,11 +85,7 @@ public class SWTexture implements Texture {
 	private int floor(float f) {
 		return (int) f;
 	}
-	/**
-	 * Not exactly ceil, but close enough for this purpose.
-	 * @param f
-	 * @return
-	 */
+
 	private int ceil(float f) {
 		return (int) Math.ceil(f);
 	}
